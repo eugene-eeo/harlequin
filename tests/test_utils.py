1439d9f8f708bf0f852a5d6f074ebaa010002229
Eeo Jun <packwolf58@gmail.com>
2016-04-03 23:51:32 +0800

    Add additional test cases

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+import pytest
+from cgi import parse_header
+from email.utils import unquote
+from harlequin.utils import want_unicode, want_bytes, generate_header
+
+
+def test_want_unicode():
+    assert want_unicode(u'∂') == u'∂'
+    assert want_unicode(u'∂'.encode('utf-8')) == u'∂'
+    assert want_unicode(u'∂'.encode('punycode'), 'punycode') == u'∂'
+
+
+def test_want_bytes():
+    assert want_bytes(u'∂') == u'∂'.encode('utf-8')
+    assert want_bytes(u'∂', 'punycode') == u'∂'.encode('punycode')
+    assert want_bytes(u'∂'.encode('punycode')) == u'∂'.encode('punycode')
+
+
+@pytest.mark.parametrize('key', ['key', 'key\'', 'key\\'])
+@pytest.mark.parametrize('val', ['val', 'val\'', 'val\\'])
+def test_generate_header(key, val):
+    header = generate_header(key, {key: val})
+    k, params = parse_header(header)
+    assert unquote(k), params == (key, {key: val})
